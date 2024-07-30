193f272de48deb941864e64b9478dff3ab27dc14
projectsMERN <projectsmern@gmail.com>
2024-07-30 19:09:11 +0530

    Update common.js

--- services/common.js
+++ services/common.js
@@ -334,22 +334,7 @@ exports.invoiceTemplate = function(order){
         <![endif]-->
         <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
 
-          <!-- start permission -->
-          <tr>
-            <td align="center" bgcolor="#D2C7BA" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
-              <p style="margin: 0;">You received this email because we received a request for [type_of_action] for your account. If you didn't request [type_of_action] you can safely delete this email.</p>
-            </td>
-          </tr>
-          <!-- end permission -->
-
-          <!-- start unsubscribe -->
-          <tr>
-            <td align="center" bgcolor="#D2C7BA" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
-              <p style="margin: 0;">To stop receiving these emails, you can <a href="https://sendgrid.com" target="_blank">unsubscribe</a> at any time.</p>
-              <p style="margin: 0;">Paste 1234 S. Broadway St. City, State 12345</p>
-            </td>
-          </tr>
-          <!-- end unsubscribe -->
+         
 
         </table>
         <!--[if (gte mso 9)|(IE)]>
@@ -653,22 +638,7 @@ exports.adminInvoiceTemplate = function(order, userEmail){
          <![endif]-->
          <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
  
-           <!-- start permission -->
-           <tr>
-             <td align="center" bgcolor="#D2C7BA" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
-               <p style="margin: 0;">You received this email because we received a request for [type_of_action] for your account. If you didn't request [type_of_action] you can safely delete this email.</p>
-             </td>
-           </tr>
-           <!-- end permission -->
- 
-           <!-- start unsubscribe -->
-           <tr>
-             <td align="center" bgcolor="#D2C7BA" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
-               <p style="margin: 0;">To stop receiving these emails, you can <a href="https://sendgrid.com" target="_blank">unsubscribe</a> at any time.</p>
-               <p style="margin: 0;">Paste 1234 S. Broadway St. City, State 12345</p>
-             </td>
-           </tr>
-           <!-- end unsubscribe -->
+           
  
          </table>
          <!--[if (gte mso 9)|(IE)]>
@@ -974,22 +944,7 @@ exports.adminInvoiceTemplate = function(order, userEmail){
          <![endif]-->
          <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
  
-           <!-- start permission -->
-           <tr>
-             <td align="center" bgcolor="#D2C7BA" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
-               <p style="margin: 0;">You received this email because we received a request for [type_of_action] for your account. If you didn't request [type_of_action] you can safely delete this email.</p>
-             </td>
-           </tr>
-           <!-- end permission -->
- 
-           <!-- start unsubscribe -->
-           <tr>
-             <td align="center" bgcolor="#D2C7BA" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
-               <p style="margin: 0;">To stop receiving these emails, you can <a href="https://sendgrid.com" target="_blank">unsubscribe</a> at any time.</p>
-               <p style="margin: 0;">Paste 1234 S. Broadway St. City, State 12345</p>
-             </td>
-           </tr>
-           <!-- end unsubscribe -->
+          
  
          </table>
          <!--[if (gte mso 9)|(IE)]>
